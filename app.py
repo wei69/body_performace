d6a411a631a84f53099bb9087623047d13125815
Wei long <75077004+wei69@users.noreply.github.com>
2025-01-23 10:39:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-image: linear-gradient(to bottom, #6dd5ed, #2193b0, #1e3c72, #3f51b5);
+        background-image: url("https://images.unsplash.com/photo-1605296867304-46d5465a13f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         background-size: cover;
         background-position: center center;
         background-attachment: fixed;
